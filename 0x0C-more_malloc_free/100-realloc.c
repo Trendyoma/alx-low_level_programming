4fe69bedd4064be674797660781cb1d4c9500b50
chioma <chukwudichiomac@gmail.com>
2023-08-10 17:14:03 -0700

    task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,8 +10,8 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *ptra
-	char ptrb;
+	char *ptra;
+	char *ptrb;
 	unsigned int i;
 
 	if (new_size == old_size)
