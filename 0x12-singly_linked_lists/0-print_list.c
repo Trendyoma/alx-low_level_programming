58033342fcd3e3909e92858ea0cb20372dfb2e64
chioma <chukwudichiomac@gmail.com>
2023-08-25 05:27:08 -0700

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_list - function that prints the element in a node
+ * @h: ponter to a structure
+ * Return: number of elements in a node
+ */
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	if (h->str == NULL)
+		printf("[%d] (%s)", 0, "(nil)");
+	while (h != NULL)
+	{
+		count++;
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	return (count);
+}
