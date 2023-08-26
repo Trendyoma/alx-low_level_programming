6f9c3ca158d953fb657f0584ca254f504c3f5a75
chioma <chukwudichiomac@gmail.com>
2023-08-26 04:39:46 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,8 +15,10 @@ size_t print_list(const list_t *h)
 	}
 	while (h)
 	{
-		if (h->str == NULL)
+		if (h->str == NULL || h == NULL)
+		{
 			h = h->next;
+		}
 		else
 		{
 		printf("[%d] %s\n", h->len, h->str);
